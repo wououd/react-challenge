0e417887150edcddf1dbed21f3476b4f7a4a4f9f
Samar AYADI <ayadisamardev@gmail.com>
2023-06-27 23:37:16 +0100

    01-Components Challenge

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-
-import               './App.css';
-import MyFirstComponent from './MyFirstComponent';
+import "./App.css";
+import Header from "./Header";
+import Post from "./Post";
+import SideMenu from "./SideMenu";
 
 function App() {
   return (
     <div className="App">
-      <MyFirstComponent />
-      <MyFirstComponent />
-      <MyFirstComponent />
-      <MyFirstComponent />
-      <MyFirstComponent />
-     
+      <Header />
+      <div style={{display: "flex" , justifyContent: "center"}} >
+        {/* POSTS & SIDE MENU CONTAINER */}
+        <div style={{ display: "flex" }}>
+          {/* POSTS CONTAINER */}
+          <div style={{ width: "70%" }}>
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+            <Post />
+          </div>
+          {/*== POSTS CONTAINER ==*/}
+
+          {/* SIDE MENU CONTAINER */}
+          <div style={{ width: "30%"}}>
+            <SideMenu />
+          </div>
+          {/*== SIDE MENU CONTAINER ==*/}
+        </div>
+        {/*== POSTS & SIDE MENU CONTAINER ==*/}
+      </div>
     </div>
   );
 }
